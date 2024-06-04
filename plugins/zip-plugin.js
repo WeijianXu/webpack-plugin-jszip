9d6e98b92c90d70b3ef8b95c6c3d27743b7f57de
WeijianXu <weijian.xu@unidt.com>
2024-06-04 23:38:13 +0800

    fix: 修复输出文件未压缩、原有文件未删除等问题；

--- plugins/zip-plugin.js
+++ plugins/zip-plugin.js
@@ -1,6 +1,7 @@
 const JSZip = require('jszip');
 const path = require('path');
 const zip = new JSZip();
+const fs = require('fs');
 const { RawSource } = require('webpack-sources');
 
 module.exports = class ZipPlugin {
@@ -12,14 +13,25 @@ module.exports = class ZipPlugin {
   }
 
   apply (compiler) {
-    compiler.hooks.emit.tapAsync('ZipPlugin', (compilation, callback) => {
+    compiler.hooks.afterCompile.tapAsync('ZipPlugin', (compilation, callback) => {
       const folderName = this.options.dir || compilation.options.output.path;
       const folder = zip.folder(folderName);
-
       for (let filename in compilation.assets) {
+        if (this.options.exclude && this.options.exclude.test(filename)) {
+          continue;
+        }
         const source = compilation.assets[filename].source();
+        folder.file(filename, source, {
+          createFolders: true
+        });
+      }
 
-        folder.file(filename, source);
+      const outDir = this.options.outDir || compilation.options.output.path;
+      const outputPath = path.join(outDir, this.options.filename + '.zip');
+
+      // 删除原来的文件
+      if (fs.existsSync(outputPath)) {
+        fs.unlinkSync(outputPath);
       }
 
       folder.generateAsync({
@@ -29,11 +41,11 @@ module.exports = class ZipPlugin {
             level: 9
         }
       }).then((content) => {
-        const outDir = this.options.outDir || compilation.options.output.path;
-        const outputPath = path.join(outDir, this.options.filename + '.zip');
 
-        const outputRelativePath = path.relative(compilation.options.output.path, outputPath);
-        compilation.assets[outputRelativePath] = new RawSource(content);
+        // const outputRelativePath = path.relative(compilation.options.output.path, outputPath);
+        // compilation.assets[outputRelativePath] = new RawSource(content);
+        // 输出到目标位置
+        require("fs").writeFile(outputPath, content, function(err){/*...*/})
 
         callback();
       });
--- src/sub/index.js
+++ src/sub/index.js
@@ -1,13 +1,15 @@
 /*
  * @Author: WeijianXu weijian.xu@unidt.com
- * @Date: 2024-04-22 11:46:33
+ * @Date: 2024-05-20 18:37:09
  * @LastEditors: WeijianXu weijian.xu@unidt.com
- * @LastEditTime: 2024-05-20 18:38:32
- * @FilePath: \output-verbatimd:\app\me\webpack-plugin-jszip\src\index.js
+ * @LastEditTime: 2024-05-20 18:37:45
+ * @FilePath: \output-verbatimd:\app\me\webpack-plugin-jszip\src\sub\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import sub from './sub/index';
-const a = [1, 2, 3];
-const b = [4, 5, 6];
-console.log(sub(a, b));
-console.log(sub(b, a));
+export default function sub(a, b) {
+  return [
+    a[0] - b[0],
+    a[1] - b[1],
+    a[2] - b[2],
+  ];
+}
